35c48212a3212f91035a2450fe84e8fb47bcf1e9
NathanC3 <njc170030@utdallas.edu>
2021-02-11 08:21:40 -0600

    Added Manual Exceptions
    Added a function to the spellcheck.py that manually adds an exception to the spelling list, which remembers exceptions by saving them to a file in data/exceptions.txt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 import nltk
 import spellcheck
 
-spell = spellcheck.SpellCheck()
+spell = spellcheck.SpellCheck('../data/exceptions.txt')
 file = open('../data/test_set.tsv', encoding='ansi')
 
-token = nltk.word_tokenize(file.read(10000))  # Trying to read the whole file is gonna take a while, don't do it
+token = nltk.word_tokenize(file.read(1000))  # Trying to read the whole file is gonna take a while, don't do it
+number, errors = spell.number_of_errors(token)
+print(number)
+print(errors)
+
+if not spell.add_exception('essay_set'):  # Demonstrating the ability to add exceptions
+    print('Error in adding exception')
+
 number, errors = spell.number_of_errors(token)
 print(number)
 print(errors)
