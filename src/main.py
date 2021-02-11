5f8c63aa14abe84f2500376d83de6b1b9d4cb64e
gautamiyer1130 <66136825+gautamiyer1130@users.noreply.github.com>
2021-02-11 16:55:18 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ import nltk
 import spellcheck
 
 spell = spellcheck.SpellCheck('../data/exceptions.txt')
-file = open('../data/test_set.tsv', encoding='ansi')
 
+try:
+    file = open('../data/test_set.tsv', encoding='ansi')
+except FileNotFoundError:
+    print("File does not exist")
+    
 token = nltk.word_tokenize(file.read(1000))  # Trying to read the whole file is gonna take a while, don't do it
 number, errors = spell.number_of_errors(token)
 print(number)
