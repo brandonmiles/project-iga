23dc1811a408e1c6f7089a12cca4127a43225438
brandonmiles <brandon.alexander.miles@gmail.com>
2021-02-12 19:26:10 -0600

    Added (non-functional) model

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+import model
 import nltk
 import spellcheck
 
 spell = spellcheck.SpellCheck()
-file = open('../data/test_set.tsv', encoding='ansi')
+test_file = open('../data/test_set.tsv', encoding='ansi')
+#train_file = open('../data/training_set.tsv', encoding='ansi')
+#valid_file = open('../data/valid_set.tsv', encoding='ansi')
 
-token = nltk.word_tokenize(file.read(10000))  # Trying to read the whole file is gonna take a while, don't do it
+test_token = nltk.word_tokenize(test_file.read(10000))  # Trying to read the whole file is gonna take a while, don't do it
 number, errors = spell.number_of_errors(token)
 print(number)
 print(errors)
+
+#mod = model.Model(100, 1)
+#mod.train(mod.get_dataframe('../data/training_set.tsv'), 64, 2)
+#mod.test(mod.get_dataframe('../data/test_set.tsv'))
--- src/model.py
+++ src/model.py
@@ -1,6 +1,37 @@
-class Model:
-	def main():
-		print("Hello world")
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+import pandas as pd
 
-if __name__ = "__main__":
-	main()
+class Model:
+	# Initialize the model
+	#
+	# num_units: Number of units in the LSTM
+	# num_layers: Number of layers in the network following the LSTM
+	def __init__(self, num_units, num_layers):
+		self.model = Sequential()
+		# TODO: Fix this mess.
+		self.model.add(LSTM(300, dropout=0.4, recurrent_dropout=0.4, input_shape=(300, 1), return_sequences=True))
+		self.model.add(LSTM(64, input_shape=(64, 10, 1), recurrent_dropout=0.4))
+		self.model.add(Dropout(0.5))
+		self.model.add(Dense(1, input_shape=(64, 1), activation='relu'))
+		self.model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['accuracy'])
+		self.model.summary()
+
+	# TODO: Move this to another file.
+	def get_dataframe(self, data_loc):
+		df = pd.read_csv(data_loc, sep='\t', encoding='ISO-8859-1')
+		df = df.dropna(axis=1)
+		if ('training_set' in data_loc):
+			df = df.drop(columns=['rater1_domain1', 'rater2_domain1'])
+			df = df.drop(columns=['essay_id', 'essay_set'])
+		return df
+
+	def train(self, training_data, batch_size, num_epochs):
+		print(training_data['essay'].shape)
+		self.model.fit(training_data['essay'], training_data['domain1_score'],
+			batch_size=batch_size, epochs=num_epochs)
+
+	def test(self, test_data):
+		self.model.predict(test_data['essay'])
