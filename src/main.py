a49178c7dd00910b03be36c2e44384978e9da226
brandonmiles <brandon.alexander.miles@gmail.com>
2021-02-12 19:33:25 -0600

    Added (presently non-functional) model

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+#import model
 import nltk
 import spellcheck
 
 spell = spellcheck.SpellCheck('../data/exceptions.txt')
 
 try:
-    file = open('../data/test_set.tsv', encoding='ansi')
+    test_file = open('../data/test_set.tsv', encoding='ansi')
+    #train_file = open('../data/training_set.tsv', encoding='ansi')
+	#valid_file = open('../data/valid_set.tsv', encoding='ansi')
 except FileNotFoundError:
     print("File does not exist")
     
-token = nltk.word_tokenize(file.read(1000))  # Trying to read the whole file is gonna take a while, don't do it
+test_token = nltk.word_tokenize(test_file.read(1000))  # Trying to read the whole file is gonna take a while, don't do it
 number, errors = spell.number_of_errors(token)
 print(number)
 print(errors)
@@ -19,3 +22,7 @@ if not spell.add_exception('essay_set'):  # Demonstrating the ability to add exc
 number, errors = spell.number_of_errors(token)
 print(number)
 print(errors)
+
+#mod = model.Model(100, 1)
+#mod.train(mod.get_dataframe('../data/training_set.tsv'), 64, 2)
+#mod.test(mod.get_dataframe('../data/test_set.tsv'))
