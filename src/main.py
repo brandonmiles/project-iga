975bf7425578fd03afe3b1833a9dcc8e18af7c60
NathanC3 <njc170030@utdallas.edu>
2021-02-24 12:07:59 -0600

    .tsv file reading
    with pandas, I can now read .tsv format correctly, reading the file as a table, check the file to see how essay reading is done, make sure to install pandas is installed first

--- src/main.py
+++ src/main.py
@@ -1,28 +1,16 @@
-#import model
 import nltk
+import pandas
 import spellcheck
 
 spell = spellcheck.SpellCheck('../data/exceptions.txt')
 
 try:
-    test_file = open('../data/test_set.tsv', encoding='ansi')
-    #train_file = open('../data/training_set.tsv', encoding='ansi')
-	#valid_file = open('../data/valid_set.tsv', encoding='ansi')
+    data = pandas.read_csv('../data/test_set.tsv', sep='\t')
+    # table column names are "essay_id", "essay_set", "essay", "domain1_predictionid", "domain2_predictionid"
+
+    token = nltk.word_tokenize(data["essay"][0])  # Reading the first essay
+    number, errors = spell.number_of_errors(token)
+    print(number)
+    print(errors)
 except FileNotFoundError:
     print("File does not exist")
-    
-test_token = nltk.word_tokenize(test_file.read(1000))  # Trying to read the whole file is gonna take a while, don't do it
-number, errors = spell.number_of_errors(token)
-print(number)
-print(errors)
-
-if not spell.add_exception('essay_set'):  # Demonstrating the ability to add exceptions
-    print('Error in adding exception')
-
-number, errors = spell.number_of_errors(token)
-print(number)
-print(errors)
-
-#mod = model.Model(100, 1)
-#mod.train(mod.get_dataframe('../data/training_set.tsv'), 64, 2)
-#mod.test(mod.get_dataframe('../data/test_set.tsv'))
