6e4f3a2e0532340ad0e98ac506abf98bd5d2047d
shresthasujan <68484009+shrsuj@users.noreply.github.com>
2021-02-26 09:26:44 -0600

    Added Grammar Checker

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from score_model import ScoreModel
 
 import pandas as pd
 import spellcheck
+import grammar_check
 
 spell = spellcheck.SpellCheck('../data/exceptions.tsv')
 
@@ -10,13 +11,19 @@ try:
 	# Table column names are "essay_id", "essay_set", "essay", "domain1_predictionid", "domain2_predictionid"
 
 	# Uncomment if you want to run the model.
-	#model = ScoreModel()
-	#model.train_and_test('../data/training_set.tsv')
+	# model = ScoreModel()
+	# model.train_and_test('../data/training_set.tsv')
 
 	for i in range(len(test_data['essay'])):
 		number, errors = spell.number_of_errors(test_data['essay'][i])
 		print("Essay: ", i, " Errors: ", number)
 		print(errors)
+
+	# Grammar Check
+	for i in range(len(test_data['essay'])):
+		number, mistakes, corrections = grammar_check.GrammarCheck.number_of_errors(test_data['essay'][i])
+		print("Essay: ", i, "Errors: ", number)
+		print(list(zip(mistakes, corrections)))
 
 except FileNotFoundError:
 	print("File does not exist")
