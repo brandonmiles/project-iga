1b1b10615cd709de98d85a0e62244d8133651b55
gautamiyer1130 <66136825+gautamiyer1130@users.noreply.github.com>
2021-03-01 16:17:35 -0600

    Update app.py

--- src/User Interface/app.py
+++ src/User Interface/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# interface.html used to display the webpage
 @app.route('/')
 def interface():
     return render_template('interface.html')
