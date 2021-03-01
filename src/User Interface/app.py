d654af5fe446a56880a378b05b198e498c67c087
gautamiyer1130 <66136825+gautamiyer1130@users.noreply.github.com>
2021-03-01 16:05:31 -0600

    Add files via upload

---
+++ src/User Interface/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def interface():
+    return render_template('interface.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
