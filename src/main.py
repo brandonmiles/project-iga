52347394079ba349918d1d9c749c0f5b571c053f
NathanC3 <njc170030@utdallas.edu>
2021-03-05 13:52:55 -0600

    Reformated to PyCharm Rules
    Just making sure the format is consistent between all the files

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from score_model import ScoreModel
 import pandas as pd
 import grade
 
-#	g = grade.Grade('../data/dictionary.tsv', 1, 1, 5)
 # How many points each graded section is worth
 rubric = {'Grammar': 20, 'Key': 20}
 # How easily or hard it is to lose points from each sections
@@ -10,12 +9,12 @@ weights = {'Grammar': 1, 'Key': 10, 'Key_Min': 2}
 g = grade.Grade('../data/dictionary.tsv', rubric, weights)
 
 try:
-    #test_data = pd.read_csv('../data/test_set.tsv', sep='\t', encoding='ISO-8859-1')
+    test_data = pd.read_csv('../data/test_set.tsv', sep='\t', encoding='ISO-8859-1')
     # Table column names are "essay_id", "essay_set", "essay", "domain1_predictionid", "domain2_predictionid"
 
     # Uncomment if you want to run the model.
     model = ScoreModel()
-    #model.train_and_test('../data/training_set.tsv')
+    # model.train_and_test('../data/training_set.tsv')
     model.test_evaluate()
 
     # Grade each essay
--- src/score_model.py
+++ src/score_model.py
@@ -3,7 +3,7 @@ from keras.layers import LSTM, Dense, Dropout
 from gensim.models import KeyedVectors
 from keras.models import Sequential
 from preprocessing import Preprocessing
-from score_model_helper import ScoreModelHelper
+import score_model_helper
 from sklearn.model_selection import KFold
 from sklearn.metrics import cohen_kappa_score
 
@@ -36,18 +36,17 @@ class ScoreModel:
         cv = KFold(n_splits=5, shuffle=True)
         results = []  # Cohen's kappa scores
         y_pred_list = []  # Actual predicted scores
-        model = None
 
-        X = ScoreModelHelper.get_dataframe(data_loc)
-        y = X['domain1_score']  # Scores from train data
+        x = ScoreModelHelper.get_dataframe(data_loc)
+        y = x['domain1_score']  # Scores from train data
 
         count = 1
-        for traincv, testcv in cv.split(X):
+        for traincv, testcv in cv.split(x):
             print("\n--------Fold {}--------\n".format(count))
-            X_test, X_train, y_test, y_train = X.iloc[testcv], X.iloc[traincv], y.iloc[testcv], y.iloc[traincv]
+            x_test, x_train, y_test, y_train = x.iloc[testcv], x.iloc[traincv], y.iloc[testcv], y.iloc[traincv]
 
-            train_essays = X_train['essay']
-            test_essays = X_test['essay']
+            train_essays = x_train['essay']
+            test_essays = x_test['essay']
 
             # Grabs all the sentences from every essay; setting up for Word2Vec
             sentences = ScoreModelHelper.get_sentences(train_essays)
@@ -66,8 +65,8 @@ class ScoreModel:
             model.init_sims(replace=True)
 
             # Preprocesses each essay into a word list
-            clean_train_essays = ScoreModelHelper.get_clean_essays(train_essays)
-            clean_test_essays = ScoreModelHelper.get_clean_essays(test_essays)
+            clean_train_essays = score_model_helper.get_clean_essays(train_essays)
+            clean_test_essays = score_model_helper.get_clean_essays(test_essays)
 
             # Preprocess the essays some more
             train_data_vecs = Preprocessing.get_avg_feature_vecs(clean_train_essays, model, num_features)
@@ -76,7 +75,7 @@ class ScoreModel:
             # Turns vectors into np arrays and reshapes them into their proper shape
             train_data_vecs = ScoreModelHelper.array_and_reshape(train_data_vecs)
             test_data_vecs = ScoreModelHelper.array_and_reshape(test_data_vecs)
-            
+
             # Train LSTM model
             lstm_model = self.get_model()
             lstm_model.fit(train_data_vecs, y_train, batch_size=64, epochs=2)
@@ -89,7 +88,7 @@ class ScoreModel:
 
             # Save any one of the 5 models
             if count == 5:
-            	lstm_model.save('./model_weights/final_lstm.h5')
+                lstm_model.save('./model_weights/final_lstm.h5')
 
             # Evaluate the model on the evaluation metric, "quadratic mean averaged kappa"
             result = cohen_kappa_score(y_test.values, y_pred, weights='quadratic')
@@ -99,33 +98,37 @@ class ScoreModel:
             count += 1
 
     def evaluate(self, essay):
-    	tk = ScoreModelHelper.load_tokenizer()
-    	text_arr = ScoreModelHelper.preprocess(essay, tk)
-    	text_arr = ScoreModelHelper.array_and_reshape(text_arr)
-    	lstm_model = self.get_model()
-    	lstm_model.load_weights('./model_weights/final_lstm.h5')
-    	y_pred = lstm_model.predict(text_arr)[0, 0]
-    	return y_pred
+        tk = ScoreModelHelper.load_tokenizer()
+        text_arr = ScoreModelHelper.preprocess(essay, tk)
+        text_arr = ScoreModelHelper.array_and_reshape(text_arr)
+        lstm_model = self.get_model()
+        lstm_model.load_weights('./model_weights/final_lstm.h5')
+        y_pred = lstm_model.predict(text_arr)[0, 0]
+        return y_pred
 
     def test_evaluate(self):
-    	print(self.evaluate('This is a test essay. It is only a test essay, and should only be referred to as such.\
-    	Any attempts to characterize this test essay as anything other than a test essay will be met with swift\
-    	punishment by the Test Essay Police (TEP).'))
-    	print(self.evaluate('Dear local newspaper, I think effects computers have on people are great learning\
-    	skills/affects because they give us time to chat with friends/new people, helps us learn about the\
-    	globe(astronomy) and keeps us out of troble! Thing about! Dont you think so? How would you feel if\
-    	your teenager is always on the phone with friends! Do you ever time to chat with your friends or buisness\
-    	partner about things. Well now - theres a new way to chat the computer, theirs plenty of sites on the\
-    	internet to do so: @ORGANIZATION1, @ORGANIZATION2, @CAPS1, facebook, myspace ect. Just think now while\
-    	your setting up meeting with your boss on the computer, your teenager is having fun on the phone not\
-    	rushing to get off cause you want to use it. How did you learn about other countrys/states outside of yours?\
-    	Well I have by computer/internet, its a new way to learn about what going on in our time! You might think your\
-    	child spends a lot of time on the computer, but ask them so question about the economy, sea floor spreading or\
-    	even about the @DATE1 s youll be surprise at how much he/she knows. Believe it or not the computer is much\
-    	interesting then in class all day reading out of books. If your child is home on your computer or at a local\
-    	library, its better than being out with friends being fresh, or being perpressured to doing something they know\
-    	isnt right. You might not know where your child is, @CAPS2 forbidde in a hospital bed because of a drive-by.\
-    	Rather than your child on the computer learning, chatting or just playing games, safe and sound in your home\
-    	or community place. Now I hope you have reached a point to understand and agree with me, because computers can\
-    	have great effects on you or child because it gives us time to chat with friends/new people, helps us learn\
-    	about the globe and believe or not keeps us out of troble. Thank you for listening.'))
+        print(self.evaluate('This is a test essay. It is only a test essay, and should only be referred to as such. '
+                            'Any attempts to characterize this test essay as anything other than a test essay will be '
+                            'met with swift punishment by the Test Essay Police (TEP).'))
+        print(self.evaluate('Dear local newspaper, I think effects computers have on people are great learning '
+                            'skills/affects because they give us time to chat with friends/new people, helps us learn '
+                            'about the globe(astronomy) and keeps us out of troble! Thing about! Dont you think so? '
+                            'How would you feel if your teenager is always on the phone with friends! Do you ever '
+                            'time to chat with your friends or buisness partner about things. Well now - theres a new '
+                            'way to chat the computer, theirs plenty of sites on the internet to do so: '
+                            '@ORGANIZATION1, @ORGANIZATION2, @CAPS1, facebook, myspace ect. Just think now while your '
+                            'setting up meeting with your boss on the computer, your teenager is having fun on the '
+                            'phone not rushing to get off cause you want to use it. How did you learn about other '
+                            'countrys/states outside of yours? Well I have by computer/internet, its a new way to '
+                            'learn about what going on in our time! You might think your child spends a lot of time '
+                            'on the computer, but ask them so question about the economy, sea floor spreading or even '
+                            'about the @DATE1 s youll be surprise at how much he/she knows. Believe it or not the '
+                            'computer is much interesting then in class all day reading out of books. If your child '
+                            'is home on your computer or at a local library, its better than being out with friends '
+                            'being fresh, or being perpressured to doing something they know isnt right. You might '
+                            'not know where your child is, @CAPS2 forbidde in a hospital bed because of a drive-by. '
+                            'Rather than your child on the computer learning, chatting or just playing games, '
+                            'safe and sound in your home or community place. Now I hope you have reached a point to '
+                            'understand and agree with me, because computers can have great effects on you or child '
+                            'because it gives us time to chat with friends/new people, helps us learn about the globe '
+                            'and believe or not keeps us out of troble. Thank you for listening.'))
