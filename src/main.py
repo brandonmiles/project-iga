3b90c779666844d633ccc005eb6b9e5267e081d7
brandonmiles <brandon.alexander.miles@gmail.com>
2021-03-05 00:54:14 -0600

    Cleaned up failed commit mess

--- src/main.py
+++ src/main.py
@@ -2,31 +2,21 @@ from score_model import ScoreModel
 import pandas as pd
 import grade
 
-<<<<<<< HEAD
 #	g = grade.Grade('../data/dictionary.tsv', 1, 1, 5)
-=======
 # How many points each graded section is worth
 rubric = {'Grammar': 20, 'Key': 20}
 # How easily or hard it is to lose points from each sections
 weights = {'Grammar': 1, 'Key': 10, 'Key_Min': 2}
 g = grade.Grade('../data/dictionary.tsv', rubric, weights)
->>>>>>> 347d22261265d01a7071a3c457f3b5540a7ea53b
 
 try:
-    #test_data = pd.read_csv('../data/test_set.tsv', sep='\t', encoding='ISO-8859-1')
+    test_data = pd.read_csv('../data/test_set.tsv', sep='\t', encoding='ISO-8859-1')
     # Table column names are "essay_id", "essay_set", "essay", "domain1_predictionid", "domain2_predictionid"
 
     # Uncomment if you want to run the model.
     model = ScoreModel()
-<<<<<<< HEAD
     #model.train_and_test('../data/training_set.tsv')
     model.test_evaluate()
-
-    # Grammar Check
-    #for i in test_data['essay']:
-        #g.get_grade(i)
-=======
-    model.train_and_test('../data/training_set.tsv')
 
     # Grade each essay
     a = 0
@@ -34,7 +24,6 @@ try:
         a = a + 1
         print("Essay: ", a)
         g.get_grade(i)
->>>>>>> 347d22261265d01a7071a3c457f3b5540a7ea53b
 
 except FileNotFoundError:
     print("File does not exist")
--- src/score_model_helper.py
+++ src/score_model_helper.py
@@ -5,7 +5,6 @@ import numpy as np
 import pandas as pd
 import pickle
 
-<<<<<<< HEAD
 # This class contains some of the helper functions used in the Model 
 # class.
 class ScoreModelHelper:
@@ -53,39 +52,3 @@ class ScoreModelHelper:
 		text_encoded = tk.texts_to_sequences([text_tokenized])
 		text_array = pad_sequences(text_encoded, maxlen=300, padding='post')
 		return text_array
-=======
-
-# This class contains some of the helper functions used in the Model class.
-
-# Returns pandas dataframe, with some dropped columns that are irrelevant for now
-def get_dataframe(data_loc):
-    df = pd.read_csv(data_loc, sep='\t', encoding='ISO-8859-1')
-    df = df.dropna(axis=1)  # Drop columns with 'missing' values
-    df = df.drop(columns=['rater1_domain1', 'rater2_domain1'])
-    df = df.drop(columns=['essay_id', 'essay_set'])
-    return df
-
-
-# Preprocesses each essays into a word list (where each element of the list is a word). This allows the essay to be
-# sequentially processed word-by-word.
-def get_clean_essays(essays):
-    clean_essays = []
-    for essay_v in essays:
-        clean_essays.append(Preprocessing.essay_to_wordlist(essay_v, remove_stopwords=True))
-    return clean_essays
-
-
-# Grabs every sentence from each essay in 'essays' and jams them into one array
-def get_sentences(essays):
-    sentences = []
-    for essay in essays:
-        sentences += Preprocessing.essay_to_sentences(essay, remove_stopwords=True)
-    return sentences
-
-
-# Turns 'data_vecs' into a np array and then reshapes it into the proper shape for LSTM fitting
-def array_and_reshape(data_vecs):
-    data_vecs = np.array(data_vecs)
-    data_vecs = np.reshape(data_vecs, (data_vecs.shape[0], 1, data_vecs.shape[1]))
-    return data_vecs
->>>>>>> 347d22261265d01a7071a3c457f3b5540a7ea53b
