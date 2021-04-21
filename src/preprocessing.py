eaa86978401d4ce26dbadd38c27e69eeeecdab8c
brandonmiles <brandon.alexander.miles@gmail.com>
2021-04-21 13:20:00 -0500

    Removed redundant functions

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -25,29 +25,3 @@ def essay_to_sentences(essay_v, remove_stopwords):
         if len(raw_sentence) > 0:
             sentences.append(essay_to_wordlist(raw_sentence, remove_stopwords))
     return sentences
-
-
-# This function computes the feature vector given some words from
-# an essay. An explanation of what a feature is may be found here:
-# https://en.wikipedia.org/wiki/Feature_(machine_learning).
-def make_feature_vec(words, model, num_features):
-    feature_vec = np.zeros((num_features,), dtype="float32")
-    num_words = 0
-    index2word_set = set(model.wv.index2word)
-    for word in words:
-        if word in index2word_set:
-            num_words += 1
-            feature_vec = np.add(feature_vec, model[word])
-    feature_vec = np.divide(feature_vec, num_words)
-    return feature_vec
-
-
-# This function computes the feature vector for each essay and
-# stores them in another vector called 'essay_feature_vecs'.
-def get_avg_feature_vecs(essays, model, num_features):
-    counter = 0
-    essay_feature_vecs = np.zeros((len(essays), num_features), dtype="float32")
-    for essay in essays:
-        essay_feature_vecs[counter] = make_feature_vec(essay, model, num_features)
-        counter += 1
-    return essay_feature_vecs
