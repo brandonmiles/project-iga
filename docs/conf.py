6303095b997e120c001fa9cac0400b92ea154702
brandonmiles <brandon.alexander.miles@gmail.com>
2021-04-28 00:38:51 -0500

    Changed documentation theme to Furo

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('../src/scripts/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +23,7 @@ copyright = '2021, Nathan Champagne, Jason Garza, Gautam Iyer, Brandon Miles, Su
 author = 'Nathan Champagne, Jason Garza, Gautam Iyer, Brandon Miles, Sujan Shrestha'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -46,7 +47,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
