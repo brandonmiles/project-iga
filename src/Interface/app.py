b00100fc2ea8bf27acd1d39f086d0e2a7d383172
gautamiyer1130 <66136825+gautamiyer1130@users.noreply.github.com>
2021-04-30 20:40:00 -0500

    Add files via upload

--- src/Interface/app.py
+++ src/Interface/app.py
@@ -11,6 +11,7 @@
 #-------------
 
 from flask import Flask, request, render_template, flash, redirect, url_for
+sys.path.insert(0, '../')
 import os
 import os.path
 from werkzeug.utils import secure_filename
